01278379f621aa0a2c3588b1544ca6dff6d327d5
Fadi Farah <fadifarah112233@gmail.com>
2022-04-18 06:32:25 +0300

    TASK-34 completed: bug fixes on the user.service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,37 +1,47 @@
-import { query } from "express";
-import { userInfo } from "os";
 import { user } from "../core/base.entities";
 
 var { UserModel } = require('./../models/user.model');
 
 exports.createUser = async function (newUser: user) {
-        try {
-            var user = await UserModel.find({email: newUser.email});
+    try {
+        var user = await UserModel.find({email: newUser.email});
 
-            if(user.length === 0) {
-                const data: any = new UserModel(newUser);
-                await data.save()
-                    .then((result: any) => {
-                        return result;
-                    })
-                    .catch((err: any) => {
-                        return err;
-                    });
-            }
-            else {
-                var user = await UserModel.findByIdAndUpdate(user[0]._id, newUser, {new: true});
-                return user;
-            }
-        } 
-        catch (e) {
-            // Log Errors
-            throw Error('Error while Paginating Users')
+        if(user.length === 0) {
+            const data: any = new UserModel(newUser);
+            await data.save()
+                .then((result: any) => {
+                    return result;
+                })
+                .catch((err: any) => {
+                    return err;
+                });
         }
+        else {
+            var tempUser: user = {
+                _id: user[0]._id,
+                displayName: newUser.displayName,
+                email: newUser.email,
+                emailVerified: newUser.emailVerified,
+                firstName: newUser.firstName,
+                lastName: newUser.lastName,
+                password: user[0].password,
+                locale: newUser.locale,
+                sub: newUser.sub,
+                picture: user[0].picture
+            }
+            var user = await UserModel.findByIdAndUpdate(user[0]._id, tempUser, {new: true});
+            return user;
+        }
+    } 
+    catch (e) {
+        // Log Errors
+        throw Error('Error while Paginating Users')
+    }
 }
 
 exports.getUserByEmail = async function (query: any) {
     try {
-        var user = await UserModel.find({query});
+        var user = await UserModel.find(query);
         return user;
     } 
     catch (e) {
